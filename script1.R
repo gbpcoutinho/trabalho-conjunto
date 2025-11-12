d00644b94257987166c0a5b24f986e7435321775
gbpcoutinho <gbpcoutinho@id.uff.br>
2025-11-12 10:23:22 -0300

    grafico de dispersao

--- script1.R
+++ script1.R
@@ -14,3 +14,11 @@ graf <- ggplot(base, aes(x = "", y = Estatura)) +
   theme_minimal(base_size = 12)
 
 graf
+
+base |> 
+  ggplot(mapping = aes(x = Peso,y = Estatura))+
+  geom_point()+
+  geom_smooth(lm="loess")+
+  theme_minimal()+
+  labs(title = "Grafico de dipersao Peso x Estatura",subtitle = paste0("correlação : ",
+                                                                       round(cor(base$Peso,base$Estatura),4)))
