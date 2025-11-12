c474be149883c35d47e8ed14fbe36b6a2dd23c53
RichardAmarante <richardmelo@id.uff.br>
2025-11-12 10:08:06 -0300

    Boxplot adicionado

--- script1.R
+++ script1.R
@@ -5,3 +5,12 @@ library(readxl)
 
 
 base = read_excel(path = "Base Casos.xlsx")
+
+summary(base)
+
+graf <- ggplot(base, aes(x = "", y = Estatura)) +
+  geom_boxplot(fill = "#10B981") +
+  labs(x = NULL, y = "Estatura (m)") +
+  theme_minimal(base_size = 12)
+
+graf
